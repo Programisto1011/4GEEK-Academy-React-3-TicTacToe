237e972e8bd4bce0362487802b7cd2c92031942b
Programisto1011 <victormaynougomez@gmail.com>
2022-01-28 13:58:44 +0000

    Need finish translate object

--- src/js/component/Square.jsx
+++ src/js/component/Square.jsx
@@ -3,26 +3,44 @@ import * as PropTypes from "prop-types";
 // include your styles into the webpack bundle
 import "../../styles/Square.css";
 
+function SquareObject(x, y) {
+	this.state = "";
+	this.position = [x, y];
+}
+SquareObject.prototype = {
+	//Si el turno es par el estado es "red" y si es impar es "blue"
+	changeColor: function (turn) {
+		console.log(turn);
+		return (this.state = parseInt(turn) % 2 == 0 ? "red" : "blue");
+	},
+};
+
 //create your first component
 const Square = (props) => {
-	const [selectedColor, setSelectedColor] = useState("");
+	const [selectedColor, setSelectedColor] = useState({
+		state: { activate: "0", color: "" },
+	});
+	// function StepTracker() {
+	// 	const [steps, setSteps] = useState(0);
+	// const increment() {
+	// 	setSteps(prevState => prevState + 1);
 	//Objeto para almacenar el estado ("", "red", "blue") y la posición
-	function SquareObject(x, y) {
-		this.state = "";
-		this.position = [x, y];
-	}
-	SquareObject.prototype = {
-		//Si el turno es par el estado es "red" y si es impar es "blue"
-		changeColor: function (turn) {
-			this.state = turn % 2 == "0" ? "red" : "blue";
-		},
-	};
 	const iSquare = new SquareObject(props.x, props.y);
 	return (
 		<>
-			onClick=
-			{() => setSelectedColor(iSquare.changeColor(props.turn))};
-			<div className={`Square ${selectedColor}`}></div>
+			<button
+				onClick={() =>
+					setSelectedColor({
+						active: "1",
+						color: iSquare.changeColor(props.turn),
+					})
+				}
+				className={`Square ${
+					selectedColor.active === "0"
+						? selectedColor.state.color
+						: ""
+				}`}></button>
+			<h1>{selectedColor}</h1>
 		</>
 	);
 };
--- src/js/view/Main.jsx
+++ src/js/view/Main.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Board from "../component/Board.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Main = () => {
 	return (
 		<>
-			<Board x="0" y="0" turn="0" />
+			<Board x="0" y="0" turn="2" />
 		</>
 	);
 };
